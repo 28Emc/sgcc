b37d3ebee70fa5b278a6a3680fb473a3b9556666
Edinson Medina Chinga <54156064+28Emc@users.noreply.github.com>
2022-03-23 10:41:14 -0500

    Creación de proyecto

---
+++ src/main/java/com/sgcc/sgccapi/SgccApiApplication.java
@@ -0,0 +1,13 @@
+package com.sgcc.sgccapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgccApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgccApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgcc/sgccapi/SgccApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgcc.sgccapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgccApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
